eaf5ebf08460be48ae1a8a01f55acb75b325e5de
chrisdude94 <christerkodehode@gmail.com>
2023-10-02 14:00:22 +0200

    *

---
+++ script.js
@@ -0,0 +1,7 @@
+let input = document.querySelector ("input");
+let myImage = document.getElementById("https://images7.alphacoders.com/493/493639.jpgs");
+let dropdown = document.getElementById("") 
+
+const img = () => {
+    result = 0;
+}
